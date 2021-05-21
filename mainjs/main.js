8812d5a4bccaf6eff3b7a577881cb710ee00fd1d
DESKTOP-G66RI1E\lhn <lhn2244@gmail.com>
2021-05-21 22:39:03 +0900

    0521_slider 보완 수정작업

--- mainjs/main.js
+++ mainjs/main.js
@@ -1,30 +1,48 @@
 // //main_page 제품소개 slide
-let gall = setInterval(main_Gallery, 2000);
-let inter = true;
-let idx = 0;
 
-function main_Gallery() {
-    if (idx < $(".main_Gallary .product_box li").length) {
-        $(".main_Gallary .product_box").animate({
-            "left": -370 * idx + "px"
-        }, 500);
-        idx++;
-    } else {
-        $(".main_Gallary .product_box").animate({
-            "left": 370 + "px"
-        }, 500);
-        idx = 0;
-    }
-}
-$(".main_Gallary").hover(function () {
-    if (inter == true) { //값이 같은지?
-        clearInterval(gall); //값이 같다면 2초 동안 작업 중지
-        inter = false; //거짓이면 다음으로 넘어가
-    }
-}, 
-    function () {
-    if (inter == false) { //inter가 거짓이면
-        gall = setInterval(main_Gallery, 2000); //main_Gallery 2초동안 실행
-        inter = true;
-    }
+$(document).ready(function () {
+
+    $(".product").after($(".product").clone()); //.gallery>ul뒤에 복제해서 붙여라
+    let position = 0; //임의변수
+    let movesize = parseInt($(".product>li").css("width")) + parseInt($(".product>li").css("margin-right")) //한개의 이미지 width값 기재함 (임의변동가능)
+    let timeonoff; //자동재생하겠음
+    let productBoxSize = parseInt($(".product_box").css("width")); //productBoxSize =4440
+
+    timeonoff = setInterval(function () { //셋인터벌 함수를 timeonoff에 넣어라
+        position -= movesize; //position 에 150을 빼서 왼쪽에 넣어라 즉 0-150 (-150씩 이동)
+        $(".product_box").stop().animate({
+            'left': position
+        }, "normal", function () {
+            if (position == -(productBoxSize / 2)) { //위치값이 -2220이 되면 (이미지의 전체너비값)
+                $(".product_box").css('left', 0); //다시 처음(왼쪽)으로 돌아와라
+                position = 0; //다 왼쪽으로 가면 다시 원위치
+            }
+        });
+    }, 2000);
+
+
+    $('.btn').click(function(){
+
+        clearInterval(timeonoff);  //슬라이드 멈춰
+    
+        let tar = $(event.target);  //tar은 target의 약자 event.target과 this는 유사함
+        if(tar.is(".lft_wrap .arrow_lft_icon")){  //m1이면 왼쪽으로 계속 이동
+            if (position == -(productBoxSize / 2)) {
+                $(".product_box").css('left',0);
+                position = 0;
+            }
+            position -= movesize;
+            $(".product_box").stop().animate({'left':position},"normal");
+        }
+        else{
+            if(position == 0){  //시작점이 po:0 으로 시작
+                $(".product_box").css('left',-2220);
+                position = -2220;
+            }
+            position += movesize;  //오른쪽버튼 누르면 150씩 오른쪽으로 이동
+            $(".product_box").stop().animate({'left':position},"normal");
+        }
+    });
 });
+
+// parseInt : css속성을 속성값으로 변환해라 즉, 350px라는 문자를 350으로 뽑아라
