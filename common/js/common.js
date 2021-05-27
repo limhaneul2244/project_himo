cea6f27bdf6e11f8074404eb962f76d9307f877d
hnlim <hnlim2244@gmail.com>
2021-05-27 23:08:40 +0900

    0527_보완작업

--- common/js/common.js
+++ common/js/common.js
@@ -1,34 +1,38 @@
-//main page_gnb_bottom_Slide 개별li마다 나타나게 하는것
-// $(document).ready(function(){
-//     $("#gnb_bottom>ul>li").mouseover(function(){
-//         $(this).children(".sub_menu").stop().slideDown();
-//     });
-//     $("#gnb_bottom>ul>li").mouseout(function(){
-//         $(this).children(".sub_menu").stop().slideUp();
-//     });
-// });
-
-// $(document).ready(function(){
-//     $("header").hover(function(){
-//         $().show()
-//     })
-// })
-
 //header slide Up, Down
 $(document).ready(function () {
     $("#gnb_bottom").hover(function () {
-            $(".sub_menu").stop().fadeIn(2000);
-            $("#slide_box").stop().slideDown();
+            $(".sub_menu").stop().fadeIn(100);
+            $("#slide_box").stop().slideDown(100);
         },
         function () {
-            $(".sub_menu").stop().fadeOut('slow');
-            $("#slide_box").stop().slideUp();
+            $(".sub_menu").stop().fadeOut(100);
+            $("#slide_box").stop().slideUp(100);
         });
 });
 //----------------------------------------------------
 
-//비디오 자동재생
-document.getElementById("vid").play();
+//video auto play
+$(document).ready(function(){
+    document.getElementById("vid").play();
+});
+
+//----------------------------------------------------
+
+// top_btn
+$(document).ready(function(){
+
+    $("#top_btn").click(function(){  //top버튼 클릭시 이동
+        $("html,body").stop().animate({"scrollTop":0},1000); //html, body 태그 기점으로 1.4초 정도 위로 이동
+    });
+    $(window).on("scroll",function(){
+        let scroll = $(window).scrollTop(); //윈도우창의 화면 높이를 측정해서
+        if(scroll>=1100){  //측정 높이가 1080이 넘어가면 기능을 수행해라
+            $("#top_btn").fadeIn('slow'); //1000이 넘어가면 버튼을 천천히 보여라
+        } else {
+            $("#top_btn").fadeOut('slow'); //1000이 넘어가지 않으면 화면에서 버튼 숨겨
+        }
+    });
+});
 //----------------------------------------------------
 
 //footer family site URL (새창열기)
@@ -36,5 +40,5 @@ function copyURL(url) {
     if (url) {
         window.open(url);
     }
-}
+};
 //----------------------------------------------------
