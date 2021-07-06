35ba0f517487ea289c6ce72e83cbb1029da48264
hnlim <hnlim2244@gmail.com>
2021-07-07 00:58:03 +0900

    0707_그누보드 연동

---
+++ sub3/foot.php
@@ -0,0 +1,192 @@
+
+            <!-- </div>
+        </div> -->
+
+        <footer id="footer">
+            <h2 class="hidden">footer</h2>
+            <div id="footer_inner">
+                <div id="footer_top">
+                    <div id="footer_menu">
+                        <ul>
+                            <li><a href="https://www.himo.co.kr/customer_03.html" target="_blank">자주묻는질문</a></li>
+                            <li><a href="https://www.himo.co.kr/member_ship.php" target="_blank">하이모 멤버십 이용약관</a></li>
+                            <li><a href="https://www.himo.co.kr/privacy.php" target="_blank">개인정보취급방침</a></li>
+                            <li><a href="https://www.himo.co.kr/agreement.php" target="_blank">하이몰 전자상거래 이용약관</a></li>
+                            <li><a href="https://www.himo.co.kr/refund_text.php" target="_blank">하이몰 교환/환불정책</a></li>
+                            <li><a href="#">제휴문의</a></li>
+                        </ul>
+                    </div>
+                    <div id="footer_sns">
+                        <div class="footer_sns_list">
+                            <span class="icon_himall"><a href="https://himomall.co.kr/" target="_blank">himall</a></span>
+                            <span class="icon_himolady"><a href="http://www.himolady.co.kr/" target="_blank">himolady</a></span>
+                            <span class="icon_blog"><a href="https://blog.naver.com/himoad1"target="_blank">naverBlog</a></span>
+                            <span class="icon_facebook"><a href="https://www.facebook.com/Himo5858/"target="_blank">faceBook</a></span>
+                            <span class="icon_youtube"><a href="https://www.youtube.com/channel/UCNyk-Gk-YXoJYTqpxT2zdJg" target="_blank">youTube</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div id="footer_logo">
+                    <div id="footer_logo_inner">
+                        <span><a href="/index.html"><img src="/common/images/footer_logo.PNG" alt="하단 로고"></a></span>
+                    </div>
+                </div>
+                <div id="footer_infor">
+                    <ul>
+                        <li>(주)HI-MO</li>
+                        <li>사업자등록정보 : 219-81-21781</li>
+                        <li>대표자 : 홍인표</li>
+                        <li class="block">본사 : 서울시 서초구 양재동 13-4 양재프라자 5층</li>
+                        <li>고객센터 : 1588-5858/080-558-5858(평일 10:00~20:00 수요일 10:00~18:00 토/일 09:00~17:00)</li>
+                        <li>통신판매업신고번호 : 제04364호</li>
+                    </ul>
+                    <p id="copy">Copyright ⓒ Hi-MO All Rights Reserved.</p>
+                </div>
+                <div id="footer_right">
+                    <select class="family_bar" onchange="copyURL(this.value)">
+                        <option value="FAMILY SITE">FAMILY SITE</option>
+                        <option value="https://himomall.co.kr/">하이몰</option>
+                        <option value="http://www.himolady.co.kr/">하이모레이디</option>
+                        <option value="https://www.hisaeng.co.kr/">하이생</option>
+                    </select>
+                </div>
+            </div>
+        </footer>
+        <div id="top_btn">
+            <a href="javascript:void(0)"><img src="/common/images/top_btn.png" alt="top_btn"></a>
+        </div>
+    </div>
+</body>
+
+</html>
+
+
+<!-- <div id="container">
+            <h2 class="hidden">container</h2>
+            <div id="section">
+                <div id="section_inner">
+                    <article class="path">
+                        <ul>
+                            <li><a href="javascript:void(0)">HI-MO뉴스</a></li>
+                            <li><a href="javascript:void(0)">HI-MO광고</a></li>
+                        </ul>
+                    </article>
+
+                    <article class="basic_tbl_wrap">
+                        <div class="tbl_inner">
+                            <table class="table">
+                                <caption>HI-MO뉴스</caption>
+                                <thead>
+                                    <tr>
+                                        <th>연번</th>
+                                        <th>제목</th>
+                                        <th>조회수</th>
+                                        <th>게시일</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td class="table_txt">
+                                            <a href="https://www.ekn.kr/web/view.php?key=20210409010001791" target="_blank">하이모, 교육 취약계층 청소년 위해 5000만원 기부</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2021-04-22</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td class="table_txt">
+                                            <a href="http://cnews.beyondpost.co.kr/view.php?ud=20201113075737380546a9e4dd7f_30" target="_blank">제7회 사랑의열매대상 서울기부자 ‘행복장·나눔장·희망장’ 수상</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-11-13</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td class="table_txt">
+                                            <a href="https://www.ekn.kr/web/view.php?key=20201102010000192" target="_blank">하이모, ‘2020 코리아세일페스타’ 참가…"풍성한 혜택 제공"</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-11-02</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td class="table_txt">
+                                            <a href="https://www.fnnews.com/news/202011060941390072" target="_blank">하이모, 소방공무원 30% 감사 할인 행사</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-11-06</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td class="table_txt">
+                                            <a href="http://www.m-i.kr/news/articleView.html?idxno=753169" target="_blank">하이모레이디, TV CF ‘머리를 쓰세요’ 온에어</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-10-12</td>
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td class="table_txt">
+                                            <a href="https://www.asiatoday.co.kr/view.php?key=20200713010007316" target="_blank">하이모, 추석맞이 고객 감사제 '맞춤가발 균일가' 진행</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-09-01</td>
+                                    </tr>
+                                    <tr>
+                                        <td>7</td>
+                                        <td class="table_txt">
+                                            <a href="http://www.newsprime.co.kr/news/article/?no=514701" target="_blank">하이모, 두피 화상 환우에 맞춤 가발 무료 지원</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-08-18</td>
+                                    </tr>
+                                    <tr>
+                                        <td>8</td>
+                                        <td class="table_txt">
+                                            <a href="https://www.news1.kr/articles/?4011311" target="_blank">하이모, 65번째 직영점 '하남지점' 오픈. 1대 1 컨설팅 솔루션 제공</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-07-30</td>
+                                    </tr>
+                                    <tr>
+                                        <td>9</td>
+                                        <td class="table_txt">
+                                            <a href="https://www.asiatoday.co.kr/view.php?key=20200713010007316" target="_blank">하이모, 맞춤가발 ‘무료체험’ 이벤트 진행</a>
+                                        </td>
+                                        <td>99</td>
+                                        <td>2020-07-13</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="paging">
+                            <div class="num">
+                                <a href="javascript:void(0)">1</a>
+                                <a href="javascript:void(0)">2</a>
+                                <a href="javascript:void(0)">3</a>
+                                <a href="javascript:void(0)">4</a>
+                                <a href="javascript:void(0)">5</a>
+                                <a href="javascript:void(0)">6</a>
+                                <a href="javascript:void(0)">7</a>
+                            </div>
+                        </div>
+
+                        <form id="board_srch" action="#" name="search_form" method="post">
+                            <fieldset class="srch">
+                                <legend class="hidden">HIMO뉴스 검색영역</legend>
+                                <label for="search_type" class="none">검색키</label>
+                                <select name="search_type" id="search_type">
+                                    <option value="title">제목</option>
+                                    <option value="contents">내용</option>
+                                </select>
+                                <label for="search_word" class="none">검색어</label>
+                                <input id="search_word" name="search_word" maxlength="30" value class="keyword" title="검색어">
+                                <input class="btn_sch btn_gray" type="submit" value="검색">
+                            </fieldset>
+                        </form>
+                    </article>
+                </div>
+            </div>
+        </div> -->
---
+++ sub3/head.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <title>HI-MO 홍보센터</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" href="/common/css/common.css">
+    <link rel="stylesheet" href="../../sub3/css/himo_sub_3.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Roboto&display=swap" rel="stylesheet">
+
+    <script src="/mainjs/jquery-3.6.0.min.js"></script>
+    <script src="/common/js/jquery-migrate-1.4.1.min.js"></script>
+    <script src="/common/js/prefixfree.min.js"></script>
+    <!-- prefixfree 접두사 제거 -->
+    <script src="/common/js/common.js"></script>
+    <script src="/common/js/header_active.js"></script>
+</head>
+
+<body>
+    <div id="skip_nav">
+        <a href="#gnb">네비게이션 바로가기</a>
+        <a href="#g5_table">홍보센터 게시판 바로가기</a>
+    </div>
+    <div id="wrap">
+        <header id="header">
+            <h2 class="hidden">header</h2>
+            <div id="header_inner">
+                <div id="main_header_top">
+                    <nav id="gnb_top">
+                        <ul>
+                            <li><a href="#">로그인</a></li>
+                            <li><a href="#">회원가입</a></li>
+                            <li><a href="#">MYPAGE</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div id="main_header_bottom">
+                    <h1 id="logo">
+                        <div id="logo_inner">
+                            <a href="/index.html">
+                                <img src="/common/images/himo_logo_maxsize.png" alt="himo logo">
+                            </a>
+                        </div>
+                    </h1>
+                    <nav id="gnb_bottom">
+                        <ul>
+                            <li><a href="/sub1/sub1-1.html">토탈헤어솔루션</a>
+                                <ul class="sub_menu">
+                                    <li><a href="#">HI-MO SYSTEM</a></li>
+                                    <li><a href="#">버추얼헤어 SYSTEM</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/sub2/sub2-1.html">하이모가이드</a>
+                                <ul class="sub_menu">
+                                    <li><a href="#">제품종류</a></li>
+                                    <li><a href="#">제품착용 및 관리법</a></li>
+                                    <li><a href="#">구매관리 및 가이드</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="https://lhn2244.cafe24.com/g5/bbs/board.php?bo_table=table_test_01">홍보센터</a>
+                                <ul class="sub_menu">
+                                    <li><a href="#">HI-MO뉴스</a></li>
+                                    <li><a href="#">HI-MO광고</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/sub4/sub4-1.html">고객서비스</a>
+                                <ul class="sub_menu">
+                                    <li><a href="#">공지사항</a></li>
+                                    <li><a href="#">이벤트</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/sub5/sub5-1.html">회사소개</a>
+                                <ul class="sub_menu">
+                                    <li><a href="#">HI-MO</a></li>
+                                    <li><a href="#">기업이념</a></li>
+                                    <li><a href="#">회사연혁</a></li>
+                                    <li><a href="#">사회공헌</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div id="slide_box"></div>
+        </header>
+
+        <div id="main_visual">
+            <h2 class="hidden">main_visual</h2>
+            <div class="mainimg">
+                <span class="mainimg_txt">홍보센터</span>
+            </div>
+        </div>
+
+        <div id="g5_table">
+            <div class="g5_table_inner">
