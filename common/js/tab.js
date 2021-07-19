e0eec5a7e2650343aaefe318ae928af7a20ce021
hnlim <hnlim2244@gmail.com>
2021-07-19 12:40:22 +0900

    0719_header, footer 인클루드

--- common/js/tab.js
+++ common/js/tab.js
@@ -1,11 +1,11 @@
 // 시각 장애인을 위한 키보드 tab 처리//
-$(document).ready(function() {        
-    $('li.gnbmenu .depth').bind('focus', function () {        
-            $('li.gnbmenu ul').slideDown('normal');
-            $('#slide_box').slideDown('fast');
-    });
-    $('li.tab5 li:last').find('a').bind('blur', function () {        
-            $('li.gnbmenu ul').slideUp('fast');
-            $('#slide_box').slideUp('normal');
-    });
+$(document).ready(function () {
+        $('li.gnbmenu .depth').bind('focus', function () {
+                $('li.gnbmenu ul').slideDown('normal');
+                $('#slide_box').slideDown('fast');
+        });
+        $('li.tab5 li:last').find('a').bind('blur', function () {
+                $('li.gnbmenu ul').slideUp('fast');
+                $('#slide_box').slideUp('normal');
+        });
 });
